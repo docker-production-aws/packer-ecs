75c9209c574dd31b93bb1e0397f1c2bc75787506
Justin Menga <justin.menga@gmail.com>
2017-11-18 14:31:22 +1300

    Add support for Docker host networking mode

--- files/firstrun.sh
+++ files/firstrun.sh
@@ -3,3 +3,19 @@ set -e
 
 # Configure ECS Agent
 echo "ECS_CLUSTER=${ECS_CLUSTER}" > /etc/ecs/ecs.config
+
+# Enable docker host networking mode if DOCKER_NETWORK_MODE is set to "host"
+if [ $DOCKER_NETWORK_MODE = "host" ]
+then
+  sudo sed -i -e "s|^\(OPTIONS=\".*\)\"$|\1 --bridge=none --ip-forward=false --ip-masq=false --iptables=false\"|" \
+    /etc/sysconfig/docker
+fi
+
+# Stop Docker service
+sudo service docker stop
+
+# Remove Docker network database
+sudo rm -rf /var/lib/docker/network
+
+# Remove docker0 interface if it has been created
+sudo ip link del docker0 || true
